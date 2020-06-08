90145687067b218c7decf5a684b29a178bc78eeb
Tom Readings <tom.readings@torchbox.com>
2020-06-08 12:54:26 +0100

    Add blog image

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,13 +2,21 @@ from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
 
     content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
+        FieldPanel('intro', classname="full"),
+        ImageChooserPanel('image'),
     ]
 
     def get_context(self, request):
@@ -29,4 +37,3 @@ class BlogPage(Page):
         FieldPanel('intro'),
         FieldPanel('body', classname="full"),
     ]
-
